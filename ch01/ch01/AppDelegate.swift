7556af33a3d1cd3faa45953ee90aee765c1572be
Debby <debby_@kakao.com>
2022-07-15 18:52:44 +0900

    220715 [Add Codes] Chapter01 완료

--- ch01/ch01/AppDelegate.swift
+++ ch01/ch01/AppDelegate.swift
@@ -13,7 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // 프로세스의 진행을 멈춰 시작 화면이 표시되는 시간을 늘려주는 역할
+        sleep(3) // second 단위 값 : 5초
         return true
     }
 
